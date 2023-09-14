23adade322776a036b766907239264ddd2c42d1c
dAvId123chcor <145074463+dAvId123chcor@users.noreply.github.com>
2023-09-14 17:24:12 -0600

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const botonAbrir = document.getElementById("abrirCerradura");
+
+    botonAbrir.addEventListener("click", function () {
+        alert("Cerradura abierta exitosamente");
+    });
+});
